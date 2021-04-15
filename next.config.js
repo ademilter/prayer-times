24cadd3c27c074470af34d95988155a763df9436
Adem ilter <ademilter@gmail.com>
2021-04-15 15:43:29 +0300

    pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
-module.exports = {
-  future: {
-    webpack5: true
+const withPWA = require('next-pwa')
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+const nextConfig = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+
+  return {
+    future: {
+      webpack5: true
+    },
+    pwa: {
+      dest: 'public',
+      disable: isDev
+    }
   }
 }
+
+module.exports = (phase) => withPWA(nextConfig(phase))
--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,74 @@ class MyDocument extends Document {
   }
 
   render() {
+    const meta = {
+      url: 'https://ezan-vakti.vercel.app',
+      name: 'Ezan Vakti',
+      description: 'Ezan Vakti'
+    }
+
     return (
-      <Html className="antialiased bg-gray-900 text-gray-50">
-        <Head />
-        <body>
+      <Html>
+        <Head>
+          <meta name="application-name" content={meta.name} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content={meta.name} />
+          <meta name="description" content={meta.description} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#000000" />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/icons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/icons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/manifest.json" />
+          <link
+            rel="mask-icon"
+            href="/static/icons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://yourdomain.com" />
+          <meta name="twitter:title" content={meta.name} />
+          <meta name="twitter:description" content={meta.description} />
+          <meta
+            name="twitter:image"
+            content="https://yourdomain.com/static/icons/android-chrome-192x192.png"
+          />
+          <meta name="twitter:creator" content="@ademilter" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={meta.name} />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:site_name" content={meta.name} />
+          <meta property="og:url" content={meta.url} />
+          <meta
+            property="og:image"
+            content="https://yourdomain.com/static/icons/apple-touch-icon.png"
+          />
+        </Head>
+
+        <body className="antialiased bg-gray-900 text-gray-50">
           <Main />
           <NextScript />
         </body>
